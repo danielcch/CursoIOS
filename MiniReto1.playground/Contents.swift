4c336ffa63ef24c8fd6953a83317818fe498483c
danielcch <mapimail@gmail.com>
2015-11-20 18:28:23 +0100

    Update Contents.swift

--- MiniReto1.playground/Contents.swift
+++ MiniReto1.playground/Contents.swift
@@ -1,19 +1,23 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
+//maximo numero
 let maximo : Int = 100
 
+//bucle desde 0 a 100 incluido
 for var numero : Int = 0 ; numero <= maximo ; numero++ {
-    
+    //si es multiplo de 5
     if(numero%5 == 0){
         print( "\(numero) Bingo!!!")
     }
-    if(numero%2 == 0){
+    
+    //si es par o impar
+    if(numero%2 == 0){ //par
         print("\(numero) Par")
-    }else{
+    }else{ // impar
         print("\(numero) Impar")
     }
+    //numero entre el rango 30 a 40 inclusive los dos
     if (numero >= 30 && numero <= 40) {
         print("\(numero) Viva Swift!!!")
     }
